76dda2cd3066ace7b88802af55432f5020569b00
Ivan5 <converse_ivan@hotmail.com>
2020-03-27 13:38:13 -0600

    Saving the image in the disk

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\domain;
+use Storage;
 use App\Category;
-use App\Domain;
-use App\Subcategory;
+use App\subcategory;
+use File;
 use Illuminate\Http\Request;
 
 class DomainController extends Controller
@@ -27,9 +29,17 @@ class DomainController extends Controller
     public function create()
     {
         $categories = Category::get();
-        $subcategories = Subcategory::get();
+        $subcategories = subcategory::get();
         return view('domain.create')->with('categories', $categories)->with('subcategories', $subcategories);
     }
+    public function paymentsuccess($payment_id)
+    {
+        $domain = domain::where('payment_id', $payment_id)->first();
+        $domain->paid = "1";
+        $domain->save();
+        return view('paidsuccess');
+    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -39,21 +49,43 @@ class DomainController extends Controller
      */
     public function store(Request $request)
     {
+
         $request->validate([
             'email' => 'required|email',
-            'title' => 'required|max:60',
-            'url' => 'required|250',
-            'shortdesc' => 'required|150'
+            'title' => 'required|max:250',
+            'url' => 'required|max:250',
+            'shortdescription' => 'required',
+            'img' => 'sometimes|image|mimes:jpeg,bmp,png,jpg,svg|max:2000',
         ]);
+        $domain = new domain();
+        $domain->title = $request['title'];
+        $domain->email = $request['email'];
+        $domain->url = $request['url'];
+        $domain->shortdescription = $request['shortdescription'];
+        $domain->description = $request['description'];
+        $domain->category_id = $request['category_id'];
+        $domain->subcategory_id = $request['subcategory_id'];
+        $domain->payment_id = uniqid();
+        if ($request['img']) {
+            $image = $request['img'];
+            $extension = $image->getClientOriginalExtension();
+            $name = time() . '_' . $image->getClientOriginalName();
+            Storage::disk('public')->put($name,  File::get($image));
+            $domain->img = $name;
+        } else {
+            $domain->img = 'default.jpg';
+        }
+        $domain->save();
+        return $domain->payment_id;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Domain  $domain
+     * @param  \App\domain  $domain
      * @return \Illuminate\Http\Response
      */
-    public function show(Domain $domain)
+    public function show(domain $domain)
     {
         //
     }
@@ -61,10 +93,10 @@ class DomainController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Domain  $domain
+     * @param  \App\domain  $domain
      * @return \Illuminate\Http\Response
      */
-    public function edit(Domain $domain)
+    public function edit(domain $domain)
     {
         //
     }
@@ -73,10 +105,10 @@ class DomainController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Domain  $domain
+     * @param  \App\domain  $domain
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Domain $domain)
+    public function update(Request $request, domain $domain)
     {
         //
     }
@@ -84,10 +116,10 @@ class DomainController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Domain  $domain
+     * @param  \App\domain  $domain
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Domain $domain)
+    public function destroy(domain $domain)
     {
         //
     }
