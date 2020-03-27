d6b994f252bef1788893bf882b964c39944aa067
Ivan5 <converse_ivan@hotmail.com>
2020-03-27 11:10:48 -0600

    Implement Vue2Editor

--- resources/js/components/DomainRegister.vue
+++ resources/js/components/DomainRegister.vue
@@ -1,38 +1,53 @@
 <template>
-  <div>
-    <form action>
-      <div class="form-group">
-        <label for="category">Select a category</label>
-        <select id="category" class="form-control"></select>
-      </div>
-      <div class="form-group">
-        <label for="subcategory">Select a subcategory if you wish</label>
-        <select id="subcategory" class="form-control"></select>
-      </div>
-      <div class="form-row">
-        <div class="form-group col-md-6">
-          <label for="email">Email</label>
-          <input type="email" class="form-control" placeholder="Please enter your email Address" />
-        </div>
-        <div class="form-group col-md-6">
-          <label for="url">Enter your Domain URL</label>
-          <input type="text" class="form-control" placeholder="http://your-domain.com" />
-        </div>
-      </div>
-      <div class="input-group mb-3">
-        <input type="file" id="img" class="form-control-file" />
-      </div>
-      <div class="form-group">
-        <label for="title">Title of your Domain</label>
-        <input type="text" class="form-control" />
-      </div>
-      <div class="form-group">
-        <label for="shortdesc">Short description</label>
-        <input type="text" class="form-control" />
-      </div>
-      <button type="submit" class="btn btn-success">Submit your Domain</button>
-    </form>
-  </div>
+    <div>
+        <form action>
+            <div class="form-group">
+                <label for="category">Select a category</label>
+                <select id="category" class="form-control"></select>
+            </div>
+            <div class="form-group">
+                <label for="subcategory"
+                    >Select a subcategory if you wish</label
+                >
+                <select id="subcategory" class="form-control"></select>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="email">Email</label>
+                    <input
+                        type="email"
+                        class="form-control"
+                        placeholder="Please enter your email Address"
+                    />
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="url">Enter your Domain URL</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        placeholder="http://your-domain.com"
+                    />
+                </div>
+            </div>
+            <div class="input-group mb-3">
+                <input type="file" id="img" class="form-control-file" />
+            </div>
+            <div class="form-group">
+                <label for="title">Title of your Domain</label>
+                <input type="text" class="form-control" />
+            </div>
+            <div class="form-group">
+                <label for="shortdesc">Short description</label>
+                <input type="text" class="form-control" />
+            </div>
+            <div class="form-group">
+                <vue-editor />
+            </div>
+            <button type="submit" class="btn btn-success">
+                Submit your Domain
+            </button>
+        </form>
+    </div>
 </template>
 <script>
 export default {};
